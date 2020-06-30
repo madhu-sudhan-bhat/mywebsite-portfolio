b93c028ecf9e56ded8e00aa1154e1ce27710da6c
madhu-sudhan-bhat <madhusudanbhat6@gmail.com>
2020-06-30 16:04:39 +0530

    Updates

---
+++ precache-manifest.31978ca2f1c56bee2c3f803f43d7b40e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c581d6cec1f94e9ee4e725fb3bfe738",
+    "url": "/mywebsite-portfolio/index.html"
+  },
+  {
+    "revision": "1ca1ae3f9e87a2126821",
+    "url": "/mywebsite-portfolio/static/css/main.9e7c1f45.chunk.css"
+  },
+  {
+    "revision": "e6e8c74434a8fd48de0a",
+    "url": "/mywebsite-portfolio/static/js/2.fd28cc08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mywebsite-portfolio/static/js/2.fd28cc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca1ae3f9e87a2126821",
+    "url": "/mywebsite-portfolio/static/js/main.60b9646b.chunk.js"
+  },
+  {
+    "revision": "13fa18672fdd0a6c8c1d",
+    "url": "/mywebsite-portfolio/static/js/runtime-main.2fd5f7b2.js"
+  }
+]);
